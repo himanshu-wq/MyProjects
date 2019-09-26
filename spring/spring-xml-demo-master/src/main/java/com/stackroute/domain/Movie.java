8d68d79b1458e55e569acbfba90f0a3637e5730f
Himanshu <Himanshu Goyal>
2019-09-26 11:56:19 +0530

    fifth

--- spring/spring-xml-demo-master/src/main/java/com/stackroute/domain/Movie.java
+++ spring/spring-xml-demo-master/src/main/java/com/stackroute/domain/Movie.java
@@ -1,13 +1,6 @@
 package com.stackroute.domain;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
-public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
+public class Movie {
     private Actor actorBean;
 
     public Movie(Actor actorBean) {
@@ -22,21 +15,4 @@ public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNam
     }
 
 
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("bean factory set");
-        System.out.println(beanFactory);
-    }
-
-    @Override
-    public void setBeanName(String s) {
-        System.out.println("bean name set");
-        System.out.println(s);
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("application context set");
-        System.out.println(applicationContext);
-    }
 }
