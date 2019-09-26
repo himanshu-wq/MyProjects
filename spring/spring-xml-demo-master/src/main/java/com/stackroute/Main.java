762bb1c728cedebe56da24261047fa8bf84731fe
Himanshu <Himanshu Goyal>
2019-09-26 11:54:18 +0530

    fourth

--- spring/spring-xml-demo-master/src/main/java/com/stackroute/Main.java
+++ spring/spring-xml-demo-master/src/main/java/com/stackroute/Main.java
@@ -2,16 +2,14 @@ package com.stackroute;
 
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args){
-        AbstractApplicationContext getMovieObj=new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext getMovieObj=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie1=getMovieObj.getBean("movieBean1",Movie.class);
         movie1.getActorDetails();
        // System.out.println(movie==movie1);
-        getMovieObj.registerShutdownHook();
     }
 
 }
