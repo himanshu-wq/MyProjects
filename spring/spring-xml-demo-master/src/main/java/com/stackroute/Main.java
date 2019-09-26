b0d027121a07dc00c0ffe6c25a0de7e0db156589
Himanshu <Himanshu Goyal>
2019-09-26 11:52:25 +0530

    third

--- spring/spring-xml-demo-master/src/main/java/com/stackroute/Main.java
+++ spring/spring-xml-demo-master/src/main/java/com/stackroute/Main.java
@@ -2,16 +2,16 @@ package com.stackroute;
 
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args){
-        ApplicationContext getMovieObj=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie=getMovieObj.getBean("movieBean",Movie.class);
-        movie.getActorDetails();
+        AbstractApplicationContext getMovieObj=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie1=getMovieObj.getBean("movieBean1",Movie.class);
         movie1.getActorDetails();
-        System.out.println(movie==movie1);
+       // System.out.println(movie==movie1);
+        getMovieObj.registerShutdownHook();
     }
 
 }
--- spring/spring-xml-demo-master/src/main/java/com/stackroute/domain/Movie.java
+++ spring/spring-xml-demo-master/src/main/java/com/stackroute/domain/Movie.java
@@ -1,12 +1,42 @@
 package com.stackroute.domain;
 
-public class Movie {
-    private Actor actorObj;
-    public Movie(Actor actorObj) {
-        this.actorObj = actorObj;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
+    private Actor actorBean;
+
+    public Movie(Actor actorBean) {
+        this.actorBean = actorBean;
     }
 
+
+
+
     public void getActorDetails(){
-         actorObj.getDetails();
+         actorBean.getDetails();
+    }
+
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("bean factory set");
+        System.out.println(beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("bean name set");
+        System.out.println(s);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("application context set");
+        System.out.println(applicationContext);
     }
 }
